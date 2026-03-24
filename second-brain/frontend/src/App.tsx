8dee5f213210edc4389f92378eccfdaa1c13d33b
Shivani Mohod <shivanimohod246@gmail.com>
2026-03-24 21:02:22 +0530

    fix routing issue

--- second-brain/frontend/src/App.tsx
+++ second-brain/frontend/src/App.tsx
@@ -1,17 +1,27 @@
 import { Signup } from "./pages/Signup"
 import { Signin } from "./pages/Signin"
-import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route , Navigate } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 
+
+
 function App() {
-  
-  return <BrowserRouter>
-    <Routes>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Default route */}
+        <Route path="/" element={<Navigate to="/signin" />} />
+
+        {/* Corrected routes */}
         <Route path="/signup" element={<Signup />} />
-        <Route path="signin" element={<Signin />} />
+        <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  </BrowserRouter>
+
+        {/* Fallback route */}
+        <Route path="*" element={<Navigate to="/signin" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
